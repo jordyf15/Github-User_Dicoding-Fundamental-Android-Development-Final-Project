69872e84bd899cdbd4a25a8cf4fc9db3fc3b76b8
jordyf15 <jordyferdian88@gmail.com>
2022-05-26 09:47:56 +0700

    initial commit

---
+++ app/src/main/java/com/jordyf15/githubuser/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jordyf15.githubuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
